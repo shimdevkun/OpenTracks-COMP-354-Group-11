ffabf0266b4ca6491d96c651ab56ee6a0192e95c
Lara Mo <laramo1999@gmail.com>
2024-03-18 23:36:08 -0400

    feat: create util class of CRUD operations and test create operation inside of TrackStoppedActivity

--- src/main/java/de/dennisguse/opentracks/TrackStoppedActivity.java
+++ src/main/java/de/dennisguse/opentracks/TrackStoppedActivity.java
@@ -1,5 +1,6 @@
 package de.dennisguse.opentracks;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,20 +8,29 @@ import android.util.Pair;
 import android.view.View;
 import android.widget.ArrayAdapter;
 
+import com.google.firestore.v1.FirestoreGrpc;
+
 import de.dennisguse.opentracks.data.ContentProviderUtils;
+import de.dennisguse.opentracks.data.UUIDUtils;
 import de.dennisguse.opentracks.data.models.ActivityType;
 import de.dennisguse.opentracks.data.models.DistanceFormatter;
 import de.dennisguse.opentracks.data.models.SpeedFormatter;
 import de.dennisguse.opentracks.data.models.Track;
+import de.dennisguse.opentracks.data.tables.TracksColumns;
 import de.dennisguse.opentracks.databinding.TrackStoppedBinding;
 import de.dennisguse.opentracks.fragments.ChooseActivityTypeDialogFragment;
 import de.dennisguse.opentracks.services.TrackRecordingServiceConnection;
+import de.dennisguse.opentracks.stats.TrackStatistics;
 import de.dennisguse.opentracks.settings.PreferencesUtils;
 import de.dennisguse.opentracks.ui.aggregatedStatistics.ConfirmDeleteDialogFragment;
 import de.dennisguse.opentracks.util.ExportUtils;
 import de.dennisguse.opentracks.util.IntentUtils;
 import de.dennisguse.opentracks.util.StringUtils;
 import de.dennisguse.opentracks.util.TrackUtils;
+import de.dennisguse.opentracks.util.TrackUtils;
+import de.dennisguse.opentracks.util.FirestoreCRUDUtil;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TrackStoppedActivity extends AbstractTrackDeleteActivity implements ChooseActivityTypeDialogFragment.ChooseActivityTypeCaller {
 
@@ -33,6 +43,7 @@ public class TrackStoppedActivity extends AbstractTrackDeleteActivity implements
     private Track.Id trackId;
 
     private boolean isDiscarding = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -105,10 +116,46 @@ public class TrackStoppedActivity extends AbstractTrackDeleteActivity implements
     }
 
     private void storeTrackMetaData(ContentProviderUtils contentProviderUtils, Track track) {
+        Map<String, Object> run = new HashMap<>();
+        TrackStatistics trackStatistics = track.getTrackStatistics();
+            if (track.getId() != null) {
+                run.put("id", track.getId().id());
+            }
+
+        run.put("ascent",  trackStatistics.hasAltitudeMax() ? trackStatistics.getMaxAltitude() : 0); // if didnt go up +Infinity
+        run.put("descent", trackStatistics.hasAltitudeMin()? trackStatistics.getMinAltitude(): 0); // if didn't go down -Infinity
+        run.put("distance", trackStatistics.getTotalDistance().toM()); // meters
+        run.put("maxSpeed", trackStatistics.getMaxSpeed().toMPS());
+        run.put("movingTime", trackStatistics.getMovingTime().toMillis()); // ms
+        run.put("stoppedTime", trackStatistics.getStopTime().toEpochMilli());
+        run.put("timerTime", trackStatistics.getMovingTime().toMillis()); //ms
+        run.put("user", "skierLara"); // TODO: get current user?
+
+        FirestoreCRUDUtil firestoreCRUD = new FirestoreCRUDUtil();
+        firestoreCRUD.createEntry("runs", run);
+
+
+ // Test
+//        Map<String, Object> getRun = new HashMap<>();
+//        getRun.put("id",65);
+//        firestoreCRUD.getEntry("runs", getRun);
+//
+//
+//        Map<String, Object> updateRun = new HashMap<>();
+//        updateRun.put("id",65);
+//        updateRun.put("ascent",1);
+//        firestoreCRUD.updateEntry("runs", getRun);
+//
+//
+//
+//        Map<String, Object> deleteRun = new HashMap<>();
+//        deleteRun.put("id",65);
+//        firestoreCRUD.deleteEntry("runs", getRun);
+
         TrackUtils.updateTrack(TrackStoppedActivity.this, track, viewBinding.trackEditName.getText().toString(),
                 viewBinding.trackEditActivityType.getText().toString(), viewBinding.trackEditDescription.getText().toString(),
                 contentProviderUtils);
-    }
+    };
 
     @Override
     public void onBackPressed() {
