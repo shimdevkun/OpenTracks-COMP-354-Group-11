dc1b4a43e15d792dec7e659bbc922df000b475f6
Axel Moran Caballero <amorancaballero@hotmail.com>
2024-03-23 02:11:58 -0400

    create and read user CRUD operations

--- src/main/java/de/dennisguse/opentracks/TrackStoppedActivity.java
+++ src/main/java/de/dennisguse/opentracks/TrackStoppedActivity.java
@@ -144,6 +144,18 @@ public class TrackStoppedActivity extends AbstractTrackDeleteActivity implements
         firestoreCRUD.createEntry(CRUDConstants.RUNS_TABLE, run);
 
 
+        //Map<String, Object> user = new HashMap<>();
+        //user.put("firstname", "test2user");
+        //user.put("lastname", "test2lastnameuser");
+        //user.put("id", "2");
+        //firestoreCRUD.createUser(user);
+
+        //firestoreCRUD.getUser("jNRwb1WmNMT4FXKh3hiq");
+
+
+
+
+
 //        TEST getEntry
 //        Map<String, Object> getRun = new HashMap<>();
 //        getRun.put("id",123);
