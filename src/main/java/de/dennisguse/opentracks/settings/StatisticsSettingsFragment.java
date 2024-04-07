639ae7ff798a3734483d398db32723499e325874
aleksandra-lelyuk <71044189+aleksandra-lelyuk@users.noreply.github.com>
2024-04-07 16:57:01 -0400

    Add plotting frequency to UI settings

--- src/main/java/de/dennisguse/opentracks/settings/StatisticsSettingsFragment.java
+++ src/main/java/de/dennisguse/opentracks/settings/StatisticsSettingsFragment.java
@@ -1,17 +1,36 @@
 package de.dennisguse.opentracks.settings;
+import de.dennisguse.opentracks.R;
+import de.dennisguse.opentracks.ui.aggregatedStatistics.dailyStats.Frequency;
 
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.preference.ListPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
-import de.dennisguse.opentracks.R;
+import java.util.List;
 
 public class StatisticsSettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         addPreferencesFromResource(R.xml.settings_statistics);
+        ListPreference freqPreference = findPreference(getString(R.string.plotting_frequency_key));
+        String[] plottingFreqEntries = new String[Frequency.values().length]; //List of frequencies
+        String[] plottingFreqValues = new String[Frequency.values().length]; //List of frequency IDs
+
+        List<Frequency> allFreqValues = List.of(Frequency.values());
+        int iterFreq = 0;
+        for (Frequency freqValue: allFreqValues) {
+            plottingFreqEntries[iterFreq] = Integer.toString(freqValue.getValue());
+            plottingFreqValues[iterFreq] = Integer.toString(freqValue.getId());
+            iterFreq++;
+        }
+        freqPreference.setEntries(plottingFreqEntries);
+        freqPreference.setEntryValues(plottingFreqValues);
+
+
+
     }
 
     @Override
