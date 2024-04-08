3523ed16ff22225435ca09265c6c0f2222251af8
TristanM2 <118041831+TristanM2@users.noreply.github.com>
2024-04-07 22:16:03 -0400

    Metric selection
    Issue #123
    Added an option to choose which default Metric to display, found in the Statistics Display in Settings.

--- src/main/java/de/dennisguse/opentracks/settings/StatisticsSettingsFragment.java
+++ src/main/java/de/dennisguse/opentracks/settings/StatisticsSettingsFragment.java
@@ -3,15 +3,32 @@ package de.dennisguse.opentracks.settings;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.preference.ListPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import de.dennisguse.opentracks.R;
+import de.dennisguse.opentracks.ui.aggregatedStatistics.dailyStats.Metric;
 
 public class StatisticsSettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         addPreferencesFromResource(R.xml.settings_statistics);
+
+        ListPreference metricPreference = findPreference(getString(R.string.plotting_metric_key));
+
+        String[] metricEntries = new String[Metric.values().length]; //Metric display names
+        String[] metricEntryValues = new String[Metric.values().length]; //Metric IDs
+
+        int iterMetric = 0;
+        for(Metric metric: Metric.values()) {
+            metricEntries[iterMetric] = metric.toString();
+            metricEntryValues[iterMetric] = Integer.toString(metric.getId());
+            iterMetric++;
+        }
+
+        metricPreference.setEntries(metricEntries);
+        metricPreference.setEntryValues(metricEntryValues);
     }
 
     @Override
