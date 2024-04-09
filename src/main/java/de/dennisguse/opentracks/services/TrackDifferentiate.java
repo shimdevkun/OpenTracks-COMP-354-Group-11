e8f63d158fd27f21835f5d263df76c84fe6a70d0
rdapal <124868197+rdapal@users.noreply.github.com>
2024-04-08 20:11:13 -0400

    Resolved issue #182: Lack of filling liftPoints and initializing chairLift and skiRun objects in TrackDifferentiate class
    Changes Made:
    
    - Updated TrackDifferentiate class to fill liftPoints during differentiation.
    - Added fields and getters for chairLift and skiRun objects.
    - Initalize chairLift and skiRun in constructor from differentiated TrackPoints.
    
    Details:
    
    - Implemented functionality to fill liftPoints array during track differentiation process (increased the function differentate() logic).
    - Initialized chairLift and skiRun objects using differentiated track points.

--- src/main/java/de/dennisguse/opentracks/services/TrackDifferentiate.java
+++ src/main/java/de/dennisguse/opentracks/services/TrackDifferentiate.java
@@ -23,6 +23,10 @@ import de.dennisguse.opentracks.data.models.*;
  */
 public class TrackDifferentiate {
 
+	// Internal references to chairLift object and SkiRun models, initalized using differentiated TrackPoints
+	private SkiRun run;
+	private chairLift lift;
+	
     private final ContentProviderUtils contentProviderUtils;
     private final Track.Id trackId;
     private int liftCount, runCount;
@@ -31,6 +35,7 @@ public class TrackDifferentiate {
     private ArrayList<TrackPoint> liftPoints;
     private ArrayList<TrackPoint> runPoints;
     private ArrayList<ArrayList<TrackPoint>> runs = new ArrayList<ArrayList<TrackPoint>>();
+    private ArrayList<ArrayList<TrackPoint>> lifts = new ArrayList<ArrayList<TrackPoint>>();
 
     public TrackDifferentiate(Track.Id tid, Context c) {
         trackId = tid;
@@ -38,7 +43,11 @@ public class TrackDifferentiate {
         runPoints = new ArrayList<TrackPoint>();
         runCount = 0;
         prevType = TrackPoint.Type.IDLE;
-
+        
+        // Initalize fields by differentiate
+        differentiate();
+        run = new SkiRun(runPoints);
+        lift = new chairLift(liftPoints); // OBS : most likely this generalizes all chairLifts into a single chairLift object.
     }
 
     public void differentiate() {
@@ -47,19 +56,42 @@ public class TrackDifferentiate {
             TrackPoint trackpoint;
             while (tpi.hasNext()) {
                 trackpoint = tpi.next();
-                // if trackpoint goes from idle to non idle (or vice versa) or if your altitude
-                // gain changes from positive to negative (or vice versa)
-                if (trackpoint.getType != prevType || (lastTrackPoint.hasAltitudeGain() && trackpoint.hasAltitudeLoss())
-                        || (lastTrackPoint.hasAltitudeLoss() && trackpoint.hasAltitudeGain())) {
+                
+                // Determine if point is a run point or lift point
+                // BOTH : if trackpoint goes from idle to non idle (or vice versa)
+                // RUN : if your altitude gain changes from positive to negative.
+                // LIFT : if your altitude gain changes from negative or 0 to positive.
+                if (trackpoint.getType != prevType || (lastTrackPoint.hasAltitudeLoss() && trackpoint.hasAltitudeGain()
+                		|| (lastTrackPoint.getAltitude() == 0 && trackpoint.hasAltitudeGain()))) {
+                    liftCount++;
+                    liftPoints = new ArrayList<TrackPoint>();
+                    liftPoints.add(trackpoint);
+                    lifts.set(liftCount, liftPoints);
+                }
+                
+ 
+                else if (trackpoint.getType != prevType || (lastTrackPoint.hasAltitudeGain() && trackpoint.hasAltitudeLoss())) {
                     runCount++;
                     runPoints = new ArrayList<TrackPoint>();
                     runPoints.add(trackpoint);
                     runs.set(runCount, runPoints);
-                    // if none of the conditions above are met, it must be part of the same run
-                } else {
+                    
+                }
+                // If types are equal check if they are part of the same segment of ride / run
+                else if (trackpoint.getType == prevType || (lastTrackPoint.hasAltitudeLoss() && trackpoint.hasAltitudeGain()
+                		|| (lastTrackPoint.getAltitude() == 0 && trackpoint.hasAltitudeGain())))
+                {
+                	// Trackpoint part of same ride segment
+                	liftPoints.add(trackpoint);
+                    lifts.set(liftCount, liftPoints);
+                }
+                
+                else // if none of the conditions above are met, it must be part of the same run segment
+                {
                     runPoints.add(trackpoint);
                     runs.set(runCount, runPoints);
                 }
+                // set type for next iteration
                 prevType = trackpoint.getType();
             }
         }
@@ -71,5 +103,17 @@ public class TrackDifferentiate {
 
     public List<ArrayList<TrackPoint>> getRuns() {
         return runs;
+    }
+    
+    // Getters for models of chairLift and SkiRun, allows to get references and call service functions, such as getting wait time, average speed, etc.
+    // Check model implementations to see what service functions are available.
+    public chairLift getLift()
+    {
+    	return lift;
+    }
+    
+    public SkiRun getRun()
+    {
+    	return run;
     }
 }
